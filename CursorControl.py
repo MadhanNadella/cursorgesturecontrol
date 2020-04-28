a117bcd9ff6eef17c266a4d9d2b61b82c7997a0e
Madhan Kumar <madhanmoni01@gmail.com>
2020-04-28 17:28:39 +0530

    #commit3

--- CursorControl.py
+++ CursorControl.py
@@ -10,10 +10,14 @@ for x, y, z in fp.main():
     z = float(z)
     # print(x,y,z)
     if y > 15:
-        pag.moveRel(5, 0)
+        pag.moveRel(5, 0, duration=0)
     if y < -15:
         # start= time.process_time()
-        pag.moveRel(-5, 0)
+        pag.moveRel(-5, 0, duration=0)
         # stop = time.process_time()
         # print(1000*(stop-start))
         # time.sleep(1)
+    if x > 12.5:
+        pag.moveRel(0, 5, duration=0)
+    if x < -15:
+        pag.moveRel(0, -5, duration=0)
